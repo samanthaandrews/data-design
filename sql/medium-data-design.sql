b3b31aa45a5f8cd6a06836448e3d020c91df32d2
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-15 22:45:27 -0700

    created "story" entity and "clap" entity in mySQL

--- sql/medium-data-design.sql
+++ sql/medium-data-design.sql
@@ -14,3 +14,35 @@ CREATE TABLE profile (
 	-- this officiates the primary key for the entity
 	PRIMARY KEY(profileId)
 );
+
+-- create the story entity
+CREATE TABLE story (
+	-- this is for yet another primary key...
+	storyId BINARY(16) NOT NULL,
+	-- this is for a foreign key
+	storyProfileId BINARY(16) NOT NULL,
+	storyContent VARCHAR(140) NOT NULL,
+	storyDateTime DATETIME(6) NOT NULL,
+	-- this creates an index before making a foreign key
+	INDEX(storyProfileId),
+	-- this creates the actual foreign key relation
+	FOREIGN KEY(storyProfileId) REFERENCES profile(profileId),
+	-- and finally create the primary key
+	PRIMARY KEY(storyId)
+);
+
+-- create the clap entity (a try hard entity from an m-to-n for profile --> story)
+CREATE TABLE clap (
+	clapId BINARY (16) NOT NULL,
+	-- these are still foreign keys
+	clapProfileId BINARY(16) NOT NULL,
+	clapStoryId BINARY(16) NOT NULL,
+	-- index the foreign keys
+	INDEX(clapProfileId),
+	INDEX(clapStoryId),
+	-- create the foreign key relations
+	FOREIGN KEY(clapProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(clapStoryId) REFERENCES story (storyId),
+	-- finally, create a composite foreign key with the two foreign keys
+	PRIMARY KEY(clapId)
+);
