ded7f067cf562dc8892ef638856a4ccc50b14062
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-22 16:58:36 -0700

    added Story namespace and class; moved json file

--- php/classes/Story.php
+++ php/classes/Story.php
@@ -1,7 +1,94 @@
 <?php
+namespace Edu\Cnm\Sandrews20\DataDesign;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+use UnexpectedValueException;
+
 /**
- * Created by PhpStorm.
- * User: sam
- * Date: 1/20/18
- * Time: 4:54 PM
- */
+ * Cross Section of a story or article written on Medium
+ *
+ * This Story class can be considered an example of what data Medium stores when users create a Story.
+ *
+ *@author Samantha Andrews <samantharaeandrews@gmail.com>
+ *@author Dylan McDonald <dmcdonald21@cnm.edu>
+ *
+ **/
+
+class Story {
+	use ValidateUuid;
+	use ValidateDate;
+	/**
+	 * id for this story; this is the primary key
+	 * @var Uuid $storyId
+	 **/
+	private $storyId;
+	/**
+	 * id of the Profile that wrote this Story; this is a foreign key
+	 * @var Uuid $storyProfileId
+	 **/
+	private $storyProfileId;
+	/**
+	 * actual textual context of this Story
+	 * @var string $storyContent
+	 **/
+	private $storyContent;
+	/**
+	 * date and time this story was published, in a PHP DateTime object
+	 * @var \DateTime $storyDateTime
+	 **/
+	private $storyDateTime;
+
+	/**constructor for this Story
+	 *
+	 * @param
+	 * @param
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 *
+	 **/
+	public function __construct($newStoryId, $newStoryProfileId, string $newStoryContent, $newStoryDateTime = null) {
+		try {
+			$this->setStoryId($newStoryId);
+			$this->setStoryProfileId($newStoryProfileId);
+			$this->setStoryContent($newStoryContent);
+			$this->setStoryDateTime($newStoryDateTime);
+		}
+	}
+
+	/**
+	 * accessor method for story id
+	 *
+	 * @return Uuid value of story id
+	 **/
+	public function getStoryId() : Uuid {
+		return ($this->storyId);
+	}
+	/**
+	 * mutator method for story id
+	 *
+	 * @param Uuid/string $newStoryId new value of story id
+	 * @throws \RangeException if $newStoryId is not positive
+	 * @throws \TypeError if $newStoryId is not a uuid or string
+	 **/
+	public function setStoryId( $newStoryId) : void {
+		try {
+			$uuid = self::validateUuid($newStoryId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception);
+	{
+	$exceptionType = get_class($exception);
+	throw(new $exceptionType($exception->getMessage(), 0, $exception));
+	}
+	// convert and store the story id
+	$this->storyId = $uuid;
+	}
+	/**
+	 *
+	 **/
+}
