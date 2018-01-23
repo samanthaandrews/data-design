0114f950020f75fa06183f6a63657f09f138225e
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-22 20:40:24 -0700

    added ValidateDate code from https://bootcamp-coders.cnm.edu/class-materials/object-oriented/object-oriented-php.php

--- php/classes/Clap.php
+++ php/classes/Clap.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sam
- * Date: 1/20/18
- * Time: 4:54 PM
- */
+namespace Edu\Cnm\Sandrews20\DataDesign;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "../vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
