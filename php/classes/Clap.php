a937ce62edb01a4920ff918650dda47a7b8039f8
Samantha Andrews <samantharaeandrews@gmail.com>
2018-01-28 21:12:05 -0700

    added Clap static functions and PDO

--- php/classes/Clap.php
+++ php/classes/Clap.php
@@ -138,6 +138,122 @@ class Clap implements \JsonSerializable {
 	}
 
 	/**
+	 * inserts this clap into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 *
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo) : void {
+		//create query
+		$query = "INSERT INTO clap(clapId, clapStoryId, clapProfileId) VALUES(:clapId, :clapStoryId, :clapProfileId)";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holders in the template
+		$parameters = ["clapId" => $this->clapId->getBytes(), "clapStoryId" => $this->clapStoryId->getBytes(), "clapProfileId" => $this->clapProfileId->getBytes()];
+		$statement->execute($parameters);
+	}
+	/**
+	 * deletes this clap in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 *
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo) : void {
+		//create query template
+		$query = "DELETE FROM clap WHERE clapId = :clapId";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holder in the template
+		$parameters = ["clapId" => $this->clapId->getBytes()];
+		$statement->execute($parameters);
+	}
+	/**
+	 * updates this clap in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 *
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo) : void {
+		//create query template
+		$query = "UPDATE clap SET clapStoryId = :clapStoryId, clapProfileId = :clapProfileId WHERE clapId = :clapId";
+		$statement = $pdo->prepare($query);
+		$parameters = ["clapId" => $this->clapId->getBytes(), "clapStoryId" => $this->clapStoryId->getBytes(), "clapProfileId" => $this->clapProfileId->getBytes()];
+		$statement->execute($parameters);
+	}
+	/**
+	 * gets clap by clapId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $clapId clap ID to search for
+	 *
+	 * @return Clap|null clap found or null if not found
+	 *
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable is not the correct data type
+	 **/
+	public static function getClapByClapId(\PDO $pdo, $clapId) : ?Clap {
+		//sanitize the ClapId before searching
+		try {
+			$clapId = self::validateUuid($clapId);
+		} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		//create query template
+		$query = "SELECT clapId, clapStoryId, clapProfileId FROM clap WHERE clapId = :clapId";
+		$statement = $pdo->prepare($query);
+		//bind the clap id to the place holder in the template
+		$parameters = ["clapId" => $clapId->getBytes()];
+		$statement->execute($parameters);
+		//grab the clap from mySQL
+		try {
+			$clap = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if ($row !== false) {
+				$clap = new Clap($row["clapId"], $row["clapStoryId"], $row["clapProfileId"]);
+			}
+		} catch (\Exception $exception) {
+			//if the row couldn't be converted, rethrow it
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($clap);
+	}
+	/**
+	 * gets all claps
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 *
+	 * @return \SplFixedArray SplFixedArray of claps found or null if not found
+	 *
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getAllClaps(\PDO $pdo) : \SplFixedArray {
+		//create query template
+		$query = "SELECT clapId, clapStoryId, clapProfileId FROM clap";
+		$statement = $pdo->prepare($query);
+		$statement->execute();
+		//build an array of claps
+		$claps = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while (($row = $statement->fetch()) !== false) {
+			try {
+				$clap = new Clap($row["clapId"], $row["clapStoryId"], $row["clapProfileId"]);
+				$claps[$claps->key()] = $clap;
+				$claps->next();
+			} catch (\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw (new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($claps);
+	}
+
+	/**
 	 * formats the state variables for JSON serialization
 	 *
 	 * @return array resulting state variables to serialize
